eb391de6cf9c8976111fa6fb5bd77b31431c69c4
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-12 23:52:47 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
+    'start_date': None,
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
