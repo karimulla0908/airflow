0ca107afb9a4929e8d5915b1254b0bf84d13c500
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-12 23:43:17 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 dag = DAG(
-    'run_databricks_notebook ',
+    'rundatabricksnotebook ',
     default_args=default_args,
     description='Databricks',
     schedule_interval=timedelta(days=1),
