ece6374a04d4b2fb418ce1b7583bf3ae4d1d75d6
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-12 23:46:40 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -15,48 +15,47 @@ default_args = {
 }
 
 dag = DAG(
-    'rundatabricksnotebook ',
+    'rundatabricksnotebook',  # Removed trailing space
     default_args=default_args,
     description='Databricks',
     schedule_interval=timedelta(days=1),
 )
 
-
 start_task = DummyOperator(
-        task_id='start_task',
-        dag=dag,
-    )
+    task_id='start_task',
+    dag=dag,
+)
 
 end_task = DummyOperator(
-        task_id='end_task',
-        dag=dag,
-    )
+    task_id='end_task',
+    dag=dag,
+)
 
 new_cluster = {
-        'spark_version': '7.6.x-scala2.12',
-        'node_type_id': 'Standard_DS3_v2',
-        'num_workers': 0,
-        'spark_conf': {
-            'spark.databricks.cluster.profile': 'singleNode',
-            'spark.master': 'local[*]'
-        },
-        'custom_tags': {
-            'TeamName': 'MLOPS Project'
-        }
+    'spark_version': '7.6.x-scala2.12',
+    'node_type_id': 'Standard_DS3_v2',
+    'num_workers': 0,
+    'spark_conf': {
+        'spark.databricks.cluster.profile': 'singleNode',
+        'spark.master': 'local[*]'
+    },
+    'custom_tags': {
+        'TeamName': 'MLOPS Project'
     }
+}
 
 notebook_task_params = {
-        'new_cluster': new_cluster,
-        'notebook_task': {
-            'notebook_path': 'Users/karimullas.de03@praxis.ac.in/Housepricepredicitionnotebook2'
-        }
+    'new_cluster': new_cluster,
+    'notebook_task': {
+        'notebook_path': 'Users/karimullas.de03@praxis.ac.in/Housepricepredicitionnotebook2'
     }
+}
 
 notebook_task = DatabricksSubmitRunOperator(
-        task_id="airflow_model_run",
-        databricks_conn_id="databricks_default",
-        dag=dag,
-        json=notebook_task_params
-    )
+    task_id="airflow_model_run",
+    databricks_conn_id="databricks_default",
+    dag=dag,
+    json=notebook_task_params
+)
 
 start_task >> notebook_task >> end_task
