64ba9db374e313710a7a396e5481f4087c47a608
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-13 01:28:34 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -44,7 +44,7 @@ sensor_task = ExternalTaskSensor(
 
 new_cluster = {
     'spark_version': '12.2.x-scala2.12',
-    'node_type_id': 'Standard_DS3_v2'
+    'node_type_id': 'Standard_DS3_v2',
     'spark_conf': {
         'spark.databricks.cluster.profile': 'singleNode',
         'spark.master': 'local[*]'
