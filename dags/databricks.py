72de0d6e736288a65403da9c00f8050075958b3d
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-13 00:48:17 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -31,21 +31,21 @@ end_task = DummyOperator(
     dag=dag,
 )
 
-# new_cluster = {
-#     'spark_version': '7.6.x-scala2.12',
-#     'node_type_id': 'Standard_DS3_v2',
-#     'num_workers': 0,
-#     'spark_conf': {
-#         'spark.databricks.cluster.profile': 'singleNode',
-#         'spark.master': 'local[*]'
-#     },
-#     'custom_tags': {
-#         'TeamName': 'MLOPS Project'
-#     }
-# }
+new_cluster = {
+    'spark_version': '7.6.x-scala2.12',
+    'node_type_id': 'Standard_DS3_v2',
+    'num_workers': 0,
+    'spark_conf': {
+        'spark.databricks.cluster.profile': 'singleNode',
+        'spark.master': 'local[*]'
+    },
+    'custom_tags': {
+        'TeamName': 'MLOPS Project'
+    }
+}
 
 notebook_task_params = {
-    'existing_cluster_id': "Karimulla Shaikh's Cluster",
+    'new_cluster': new_cluster,
     'notebook_task': {
         'notebook_path': '/Workspace/Users/karimullas.de03@praxis.ac.in/Housepricepredicitionnotebook2'
     }
@@ -55,7 +55,8 @@ notebooktask = DatabricksSubmitRunOperator(
     task_id="airflowmodelrun",
     databricks_conn_id="databricks_default",
     dag=dag,
-    json=notebook_task_params
+    json=notebook_task_params,
+    timeout_seconds=3600
 )
 
 start_task >> notebooktask >> end_task
