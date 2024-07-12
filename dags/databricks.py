4fed00a8780e4d23dc9904123793f3e1ee67110c
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-07-13 02:10:58 +0530

    Update databricks.py

--- dags/databricks.py
+++ dags/databricks.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.sensors.base import BaseSensorOperator
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 from datetime import datetime, timedelta
 
@@ -32,36 +31,6 @@ end_task = DummyOperator(
     dag=dag,
 )
 
-# Define CustomSensor to wait for the completion of the prerequisite DAG
-class ExternalTaskCompletionSensor(BaseSensorOperator):
-    def __init__(self, external_dag_id, external_task_id, execution_delta, *args, **kwargs):
-        self.external_dag_id = external_dag_id
-        self.external_task_id = external_task_id
-        self.execution_delta = execution_delta
-        super().__init__(*args, **kwargs)
-
-    def poke(self, context):
-        execution_date = context['execution_date']
-        external_dag_run = self.get_dagrun(
-            self.external_dag_id,
-            execution_date - self.execution_delta,
-            execution_date + self.execution_delta
-        )
-        if not external_dag_run:
-            return False
-        external_task_instance = external_dag_run.get_task_instance(self.external_task_id)
-        return external_task_instance.is_complete()
-
-sensor_task = ExternalTaskCompletionSensor(
-    task_id='wait_for_completion_of_other_dag',
-    external_dag_id='hello_world',  # Replace with the ID of the prerequisite DAG
-    external_task_id='say_hello',  # Replace with the task ID in the other DAG to wait for
-    execution_delta=timedelta(seconds=20),  # Wait for 20 seconds after completion
-    timeout=60,  # Timeout after 1 hour if the other DAG doesn't complete
-    poke_interval=30,  # Check every 30 seconds for task completion
-    dag=dag,
-)
-
 new_cluster = {
     'spark_version': '12.2.x-scala2.12',
     'node_type_id': 'Standard_DS3_v2',
@@ -90,4 +59,4 @@ notebooktask = DatabricksSubmitRunOperator(
 )
 
 # Define the task dependency
-sensor_task >> start_task >> notebooktask >> end_task
+start_task >> start_task >> notebooktask >> end_task
